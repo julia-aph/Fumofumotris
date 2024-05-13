8b482a0bb66d0950c2bb029e51d691b46927d241
Julia <145168563+julia-aph@users.noreply.github.com>
2024-05-13 02:11:02 -0500

    df
    df

--- build.py
+++ build.py
@@ -11,18 +11,19 @@ ARGS = "-fdiagnostics-color -pthread -Wall -std=c17 -pedantic -g"
 
 SOURCE_DIR = Path("source/")
 OBJECT_DIR = Path("objects/")
-OUTPUT = Path("debug")
+OUTPUT = Path("build/debug")
 
 
-CHECKSUMS = Path("checksums.txt")
-ERRORS = Path("errors.txt")
+SRC_CHECKSUMS_TXT = Path("build/src_checksums.txt")
+HDR_CHECKSUMS_TXT = Path("build/hdr_checksums.txt")
+ERRORS_TXT = Path("build/errors.txt")
 
 
-def scan_checksums(files: list[Path]) -> list[str]:
-    return (md5(file.read_bytes()).hexdigest() for file in files)
+def scan_checksums(files) -> list[str]:
+    return [md5(file.read_bytes()).hexdigest() for file in files]
 
 
-def read_txt(txt: Path) -> any:
+def read_text(txt: Path) -> any:
     if not txt.exists():
         return []
     
@@ -30,84 +31,108 @@ def read_txt(txt: Path) -> any:
     return json.loads(content) if content else []
 
 
-def write_txt(txt: Path, content) -> None:
+def write_text(txt: Path, content) -> None:
     txt.write_text(json.dumps(content))
 
 
-def difference(cur, old, vals):
-    return (vals[cur.index(i)] for i in set(cur) - set(old))
+def checksum_difs(cur, old, files) -> set[str]:
+    dif = set(cur) - set(old)
+    return ({files[cur.index(i)] for i in dif}, dif)
 
 
-def clean_objects(object_dir: Path, sources: list[Path]) -> None:
-    objects: list[Path] = [object for object in object_dir.rglob("*.o")]
+def delete_unused_objects(object_dir: Path, source_stems: list[str]) -> None:
+    objects = [object for object in object_dir.rglob("*.o")]
 
-    object_stems = [object.stem for object in objects]
-    source_stems = [source.stem for source in sources]
-
-    for stem in set(object_stems).difference(source_stems):
-        objects[object_stems.index(stem)].unlink()
+    map(Path.unlink, [object for object in objects if object.stem not in source_stems])
 
 
-def header_dependencies(sources):
-    tasks = []
+def paths_to_args(args: list[Path], sep: str="") -> str:
+    return " ".join(f"{sep}{arg}" for arg in args)
+
+
+def dispatch(args):
+    return subprocess.Popen(args, stdout=PIPE, stderr=PIPE, text=True, encoding="utf-8")
+
+
+def header_deps(sources, includes):
+    incl = paths_to_args(includes, "-I ")
+    src = paths_to_args(sources)
+    out, err = dispatch(f"{GCC} -MM {src} {ARGS} {incl}").communicate()
+
+    dependencies = []
+    if out:
+        for line in out.splitlines():
+            if ":" in line:
+                dependencies.append(set())
+
+            dependencies[-1].add(Path(line.strip(" \\")))
+
+    return dependencies
+
+
+def compile_job(sources, includes, errors, object_dir):
+    incl = paths_to_args(includes, "-I ")
+
+    processes = []
     for source in sources:
-        task = subprocess.Popen(f"{GCC} -MMD {source} {ARGS} -o stdout", stdout=PIPE)
-        tasks.append(task)
+        output = object_dir.joinpath(source.with_suffix(".o").name)
 
-    for task in tasks:
-        
+        processes.append(dispatch(f"{GCC} -c {source} {ARGS} {incl} -o {output}"))
 
-
-def compile(source: Path, includes: list[Path], object_dir: Path):
-    include_arg: str = " ".join(f"-I {dir}" for dir in includes)
-    output: Path = object_dir / source.with_suffix(".o").name
-
-    args = f"{GCC} -c {source} {ARGS} {include_arg} -o {output}"
-
-    return subprocess.Popen(args, stderr=subprocess.PIPE)
+    for process, source in zip(processes, sources):
+        out, err = process.communicate()
+        errors[source.stem] = err
 
 
-def wait_compile_tasks(tasks, updated_sources) -> dict[str, str]:
-    errors = disk_read_errors(ERRORS)
-
-    for task, source in zip(tasks, updated_sources):
-        out, err = task.communicate()
-        if err:
-            errors[str(source)] = err.decode("utf-8")
-        else:
-            errors[str(source)] = False
-
-    disk_write_errors(ERRORS, errors)
-    return errors
-
-
-def link(object_dir: Path, output: Path) -> None:
-    subprocess.run(f"{GCC} {object_dir}/*.o {ARGS} -o {output}")
+def link_job(object_dir, output):
+    out, err = dispatch(f"{GCC} {object_dir}/*.o {ARGS} -o {output}").communicate()
+    return err
 
 
 def build(src: Path, object_dir: Path, output: Path):
-    includes, headers, sources = zip(map(src.rglob, ["*/", "*.h", "*.c"]))
+    # Walk source directory, obtain checksums
+    includes, headers, sources = map(list, map(src.rglob, ["*/", "*.h", "*.c"]))
 
-    headers_cur, sources_cur = map(scan_checksums, (headers, sources))
-    headers_old, sources_old = read_txt(CHECKSUMS)
+    hc_cur, sc_cur = scan_checksums(headers), scan_checksums(sources)
+    hc_old, sc_old = read_text(HDR_CHECKSUMS_TXT), read_text(SRC_CHECKSUMS_TXT)
 
-    headers_updt = difference(headers_cur, headers_old, headers)
-    sources_updt = difference(sources_cur, sources_old, sources)
+    # Find out which sources need to be compiled based on checksum differences
+    # and dependencies on changed headers
+    header_updates, hc_updates = checksum_difs(hc_cur, hc_old, headers)
+    source_updates, sc_updates = checksum_difs(sc_cur, sc_old, sources)
 
-    dependencies = {}
+    for source_dependencies, source in zip(header_deps(sources, includes), sources):
+        if any(header in header_updates for header in source_dependencies):
+            source_updates.add(source)
 
-    tasks = []
-    for source in updated_sources:
-        tasks.append(compile(source, includes, object_dir))
+    # Compile step: Read old error messages, then update, print, and write them
+    errors = dict(read_text(ERRORS_TXT))
+    compile_job(source_updates, includes, errors, object_dir)
 
-    errors = wait_compile_tasks(tasks, updated_sources).values()
+    error_amt = 0
+    source_stems = [source.stem for source in sources]
+    for source_stem, message in list(errors.items()):
+        if source_stem not in source_stems:
+            errors.pop(source_stem)
+        elif message:
+            print(message)
+            error_amt += 1
+        else:
+            sc_updates.remove(sc_cur[source_stems.index(source_stem)])
 
-    print("\n".join(err for err in errors if err is not False).strip("\n"))
+    write_text(HDR_CHECKSUMS_TXT, list(hc_updates))
+    write_text(SRC_CHECKSUMS_TXT, list(sc_updates))
+    write_text(ERRORS_TXT, errors)
 
+    # Link step: Delete unused objects, link and print the error message
+    delete_unused_objects(object_dir, [source.stem for source in sources])
+    link_err = link_job(object_dir, output)
 
-    clean_objects(object_dir, all_sources)
-    link(object_dir, output)
-    print(f"Compiled: {len(updated_sources)} Linked: {len(all_sources)}")
+    if link_err:
+        print(link_err)
+
+    # yippee
+    print(f"Compiled: {len(source_updates)} Linked: {len(sources)} Errored: {error_amt}")
 
 
 if __name__ == "__main__":
